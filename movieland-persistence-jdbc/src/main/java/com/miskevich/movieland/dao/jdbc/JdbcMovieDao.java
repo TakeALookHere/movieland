3e1dc701250f2f630e568af3434216f072cf48c1
juliamanunia <julia.manunia@yahoo.com>
2017-10-29 16:12:31 +0200

    add JdbcDaoTest

--- movieland-persistence-jdbc/src/main/java/com/miskevich/movieland/dao/jdbc/JdbcMovieDao.java
+++ movieland-persistence-jdbc/src/main/java/com/miskevich/movieland/dao/jdbc/JdbcMovieDao.java
@@ -7,11 +7,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Service
+@Repository
 public class JdbcMovieDao implements IMovieDao {
 
     private final Logger LOG = LoggerFactory.getLogger(getClass());
--- movieland-persistence-jdbc/src/test/java/com/miskevich/movieland/dao/jdbc/JdbcMovieDaoTest.groovy
+++ movieland-persistence-jdbc/src/test/java/com/miskevich/movieland/dao/jdbc/JdbcMovieDaoTest.groovy
@@ -1,11 +1,17 @@
 package com.miskevich.movieland.dao.jdbc
 
 import com.miskevich.movieland.entity.Movie
+import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests
 import org.testng.annotations.Test
 
 import static org.testng.Assert.assertNotNull
 
+@ContextConfiguration(locations = "classpath:/spring/jdbc-context.xml")
+//@RunWith(SpringJUnit4ClassRunner.class)
 class JdbcMovieDaoTest {
 
     @Autowired
